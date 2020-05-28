81b8b1e4a36c3f42262aeba2e453567e4edafe47
NikiSoft <jn.nikisoft@gmail.com>
2020-05-28 12:13:52 +0200

    Fixed build of library

--- projects/ns-js-utils/src/lib/dates/ns-date-time.ts
+++ projects/ns-js-utils/src/lib/dates/ns-date-time.ts
@@ -1,4 +1,4 @@
-import * as moment from 'moment';
+import moment from 'moment';
 import { nsStringJoin } from '../helpers/strings/ns-helpers-strings';
 import { NsDate } from './ns-date';
 
--- projects/ns-js-utils/src/lib/dates/ns-date.ts
+++ projects/ns-js-utils/src/lib/dates/ns-date.ts
@@ -1,4 +1,4 @@
-import * as moment from 'moment';
+import moment from 'moment';
 import { nsIsNotNullOrEmpty } from '../helpers/strings/ns-helpers-strings';
 
 export class NsDate extends Object {

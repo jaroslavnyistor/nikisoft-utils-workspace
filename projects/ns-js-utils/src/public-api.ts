bfb945032401bb3d7631df964b8ed4a23e39d77d
NikiSoft <jn.nikisoft@gmail.com>
2020-05-28 14:48:36 +0200

    entrypoint.ts renamed to index.ts to have exported members and added description to public-api.ts

--- projects/ns-js-utils/src/public-api.ts
+++ projects/ns-js-utils/src/public-api.ts
@@ -1,4 +1,11 @@
 /*
  * Public API Surface of ns-js-utils
+ *
+ * To generate all public-api as one index.ts do following:
+ * 1. npm install create-ts-index --save-dev
+ * 2. Open terminal at src folder of library
+ * 3. Run cti entrypoint -n ./lib
+ * 4. Rename file entrypoint.ts to index.ts
+ * 5. Added here export * from './lib/index';
  */
-export * from './lib/entrypoint';
+export * from './lib/index';

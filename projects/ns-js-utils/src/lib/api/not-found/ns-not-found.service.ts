428aa9436b89fe7d4f37002a34b09091f112339a
NikiSoft <jn.nikisoft@gmail.com>
2020-06-01 06:50:12 +0200

    Lib - Renamed ns-page-not-found.service.ts to ns-not-found.service.ts

--- projects/ns-js-utils/src/lib/api/not-found/ns-not-found.service.ts
+++ projects/ns-js-utils/src/lib/api/not-found/ns-not-found.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
 import { NsRouterService } from '../../navigation/ns-router.service';
-import { routeNotFound } from './ns-page-not-found.route';
+import { routeNotFound } from './ns-not-found.route';
 
 @Injectable({
   providedIn: 'root',
 })
-export class NsPageNotFoundService {
+export class NsNotFoundService {
   constructor(private _routerService: NsRouterService) {}
 
   navigate() {

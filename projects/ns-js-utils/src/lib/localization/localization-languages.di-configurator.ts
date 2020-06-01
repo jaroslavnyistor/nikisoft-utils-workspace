727a53801a785830e3871291649c094384c2c1de
NikiSoft <jn.nikisoft@gmail.com>
2020-06-01 06:51:06 +0200

    Lib - Created function provideResolveLanguage to resolveLanguage and can be used in decorators like providers field

--- projects/ns-js-utils/src/lib/localization/localization-languages.di-configurator.ts
+++ projects/ns-js-utils/src/lib/localization/localization-languages.di-configurator.ts
@@ -3,6 +3,10 @@ import { DI_NS_DEFAULT_LANGUAGE, DI_NS_USES_LOCALIZATION } from './localization-
 import { LocalizationLanguagesService } from './localization-languages.service';
 import { LocalizationLanguage } from './localization.language';
 
+export function provideResolveLanguage() {
+  return () => LocalizationLanguagesService.resolveLanguage();
+}
+
 export function localizationLanguagesServiceAppInitializer(service: LocalizationLanguagesService) {
   return () => service.load();
 }

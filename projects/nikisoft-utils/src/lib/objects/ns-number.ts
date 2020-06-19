919f8ace9ea2f0256e7640b0b77ee1aff0f3afa7
NikiSoft <jn.nikisoft@gmail.com>
2020-06-19 07:45:31 +0200

    Lib - NsArray parseIntFromArray method helps to parse string at index in array to number with defaultValue

--- projects/nikisoft-utils/src/lib/objects/ns-number.ts
+++ projects/nikisoft-utils/src/lib/objects/ns-number.ts
@@ -1,6 +1,10 @@
+import { NsArray } from './ns-array';
+
 export class NsNumber {
   static formatFloatNumber(value: number, digits = 2): string {
-    return value == null ? null : value.toLocaleString(undefined, { maximumFractionDigits: digits, useGrouping: false });
+    return value == null
+      ? null
+      : value.toLocaleString(undefined, { maximumFractionDigits: digits, useGrouping: false });
   }
 
   static formatNumber(value: number, digits = 2): string {
@@ -19,5 +23,11 @@ export class NsNumber {
 
   static roundNumberUp(value: number, decimals: number): number {
     return Number(Math.ceil(Number(`${value}e${decimals}`)) + 'e-' + decimals);
+  }
+
+  static parseIntFromArray(items: string[], index: number, defaultValue = 0) {
+    const value = NsArray.itemAt(items, index);
+
+    return value == null ? defaultValue : Number.parseInt(value, 10);
   }
 }

f564d9f7fdfc95051df25ce21e656103bc054b59
NikiSoft <jn.nikisoft@gmail.com>
2020-06-19 07:45:57 +0200

    Code cleanup

--- projects/nikisoft-utils/src/lib/objects/ns-language.ts
+++ projects/nikisoft-utils/src/lib/objects/ns-language.ts
@@ -3,8 +3,7 @@ export class NsLanguage {
     let language;
     if (window.navigator.languages) {
       language = window.navigator.languages[0];
-    }
-    else {
+    } else {
       language = window.navigator.language;
     }
 
